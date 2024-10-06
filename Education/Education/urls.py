e16ece4a3e86d598afaf90198c0691f3988eaebb
Saran <saran152004s@gmail.com>
2024-10-06 06:14:14 +0530

    backend
    logical coding par updated

---
+++ Education/Education/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls, name="admin"),
+    path('', include('edu.urls')),
+
+]
